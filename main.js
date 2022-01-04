5ac5d518a36860be52a218fb4062f58c71be6726
Interact-fan <95719545+Interact-fan@users.noreply.github.com>
2022-01-04 09:44:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ if (z > x) {
 } else if (y > z) {
   document.getElementById("salt221").innerHTML = 98 * 21;
 }
+if (x > g {
+    while (y > z) {
+document.getElementById('salt112').style.backgroundcolor='blue';
+document.getElementById('salt112').style.backgroundcolor='lightblue';
+document.getElementById('salt112').style.backgroundcolor='green';
+document.getElementById('salt112').style.backgroundcolor='red';
+document.getElementById('salt112').style.backgroundcolor='yellow';
+  }
+}
